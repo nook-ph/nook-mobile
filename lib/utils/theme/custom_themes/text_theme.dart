45923cf3b581a9a235b4d3558d10574afc713f2b
aeqqj <48713075+aeqqj@users.noreply.github.com>
2026-02-20 22:41:07 +0800

    feat: added text and color schemes

--- lib/utils/theme/custom_themes/text_theme.dart
+++ lib/utils/theme/custom_themes/text_theme.dart
@@ -1,21 +1,95 @@
 import 'package:flutter/material.dart';
 
-
-
 class TTextTheme {
-    TTextTheme._();
+  TTextTheme._();
 
-    static TextTheme lightTextTheme = TextTheme(
-      headlineLarge: TextStyle().copyWith(fontSize: 32.0, fontWeight: FontWeight.w600, color: Colors.black),
-      headlineMedium: TextStyle().copyWith(fontSize: 24.0, fontWeight: FontWeight.w600, color: Colors.black),
+  static TextTheme lightTextTheme = TextTheme(
+    headlineLarge: const TextStyle(
+      fontSize: 48.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.black,
+    ),
 
+    headlineMedium: const TextStyle(
+      fontSize: 40.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.black,
+    ),
 
-      titleLarge: TextStyle().copyWith(fontSize: 20.0, fontWeight: FontWeight.w500, color: Colors.black),
-      titleMedium: TextStyle().copyWith(fontSize: 18.0, fontWeight: FontWeight.w500, color: Colors.black),
-      titleSmall: TextStyle().copyWith(fontSize: 14.0, fontWeight: FontWeight.w500, color: Colors.black),
+    headlineSmall: const TextStyle(
+      fontSize: 32.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.black,
+    ),
 
-      bodyLarge: TextStyle().copyWith(fontSize: 15.0, fontWeight: FontWeight.normal, color: Colors.black),
-      bodyMedium: TextStyle().copyWith(fontSize: 14.0, fontWeight: FontWeight.normal, color: Colors.black),
-      bodySmall: TextStyle().copyWith(fontSize: 12.0, fontWeight: FontWeight.normal, color: Colors.black),
-    );
+    titleLarge: const TextStyle(
+      fontSize: 24.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.black,
+    ),
+
+    titleMedium: const TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.black,
+    ),
+
+    titleSmall: const TextStyle(
+      fontSize: 18.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.black,
+    ),
+
+    bodyLarge: const TextStyle(
+      fontSize: 16.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.black,
+    ),
+
+    bodyMedium: const TextStyle(
+      fontSize: 14.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.black,
+    ),
+
+    bodySmall: const TextStyle(
+      fontSize: 12.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.black,
+    ),
+  );
+}
+
+extension CustomText on TextTheme {
+  TextStyle get headlineLargeEmp =>
+      headlineLarge!.copyWith(fontWeight: FontWeight.w500);
+
+  TextStyle get headlineMediumEmp =>
+      headlineMedium!.copyWith(fontWeight: FontWeight.w500);
+
+  TextStyle get headlineSmallEmp =>
+      headlineSmall!.copyWith(fontWeight: FontWeight.w500);
+
+  TextStyle get titleLargeEmp =>
+      titleLarge!.copyWith(fontWeight: FontWeight.w500);
+
+  TextStyle get titleMediumEmp =>
+      titleMedium!.copyWith(fontWeight: FontWeight.w500);
+
+  TextStyle get titleSmallEmp =>
+      titleSmall!.copyWith(fontWeight: FontWeight.w500);
+
+  TextStyle get bodyLargeEmp =>
+      bodyLarge!.copyWith(fontWeight: FontWeight.w500);
+
+  TextStyle get bodyMediumEmp =>
+      bodyMedium!.copyWith(fontWeight: FontWeight.w500);
+
+  TextStyle get bodySmallEmp =>
+      bodySmall!.copyWith(fontWeight: FontWeight.w500);
+
+  TextStyle get bodyExtraSmall => bodySmall!.copyWith(fontSize: 12.0);
+
+  TextStyle get bodyExtraSmallEmp =>
+      bodyExtraSmall.copyWith(fontWeight: FontWeight.w500);
 }
