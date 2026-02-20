974aaedc13dbfd3b0b5cee4c7d844027d9f2293e
lucerocris <lucerocris22@gmail.com>
2026-02-21 01:14:00 +0800

    feat/refactor: extracted widgets to its own file, and added menu highlights in cafe details

---
+++ lib/features/cafe_details/presentation/widgets/menu_highlights.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class MenuHighlights extends StatelessWidget {
+  const MenuHighlights({super.key, required this.width});
+
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 22),
+          child: Text(
+            'Menu Highlights',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+              color: Colors.black,
+            ),
+          ),
+        ),
+
+        const SizedBox(height: 16),
+
+        SizedBox(
+          height: 178,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.symmetric(horizontal: 22),
+            itemCount: 5,
+            separatorBuilder: (context, index) => const SizedBox(width: 12),
+            itemBuilder: (context, index) {
+              return Container(
+                width: width,
+                height: 178,
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(
+                    color: const Color(0xFFE0E0E0),
+                    width: 1.0,
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: Image.network(
+                        'https://images.unsplash.com/photo-1497935586351-b67a49e012bf',
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+
+                    Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: EdgeInsets.all(12),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Iced Spanish Latte',
+                              style: TextStyle(fontSize: 14),
+                            ),
+
+                            Gap(2),
+
+                            Text('₱150.00', style: TextStyle(fontSize: 14)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
