0b16d10acfef5c9732ac1864bbbf1947041e9533
Cris Lucero <lucerocris22@gmail.com>
2026-05-24 22:22:21 +0800

    chore: added onboarding images

--- lib/features/onboarding/data/onboarding_data.dart
+++ lib/features/onboarding/data/onboarding_data.dart
@@ -1,26 +1,34 @@
 class OnboardingModel {
-    final String title;
-    final String description;
+  final String title;
+  final String description;
+  final String imagePath;
 
-    const OnboardingModel({
-        required this.title,
-        required this.description
-    });
+  const OnboardingModel({
+    required this.title,
+    required this.description,
+    required this.imagePath,
+  });
 }
 
 class OnboardingData {
   static const List<OnboardingModel> items = [
     OnboardingModel(
       title: "Find your\nPerfect Brew",
-      description: "Discover local hidden gems, rate your favorites, and never settle for bad coffee again",
+      description:
+          "Discover local hidden gems, rate your favorites, and never settle for bad coffee again",
+      imagePath: "assets/images/relaxing.png",
     ),
     OnboardingModel(
       title: "Explore Local\nCafes",
-      description: "Find the best spots for studying, meetings, or just a quick coffee with our interactive map",
+      description:
+          "Find the best spots for studying, meetings, or just a quick coffee with our interactive map",
+      imagePath: "assets/images/looking.png",
     ),
     OnboardingModel(
       title: "Join the Coffee\nCommunity",
-      description: "Rate your favorites, save spots for later, and help others find the best study nooks",
+      description:
+          "Rate your favorites, save spots for later, and help others find the best study nooks",
+      imagePath: "assets/images/friends.png",
     ),
   ];
 }
--- lib/features/onboarding/presentation/widgets/onboarding_image.dart
+++ lib/features/onboarding/presentation/widgets/onboarding_image.dart
@@ -2,25 +2,22 @@ import 'package:flutter/material.dart';
 import '../../data/onboarding_data.dart';
 
 class OnboardingImageWidget extends StatelessWidget {
-    final OnboardingModel model;
+  final OnboardingModel model;
 
-    const OnboardingImageWidget({super.key, required this.model});
+  const OnboardingImageWidget({super.key, required this.model});
 
-    @override
-    Widget build(BuildContext context) {
-     
-        return Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration(
-              color: Colors.grey.shade200,
-              shape: BoxShape.rectangle,
-              borderRadius: BorderRadius.circular(20)
-            ),
-
-            child: const Icon(Icons.image, size: 60, color: Colors.grey)
-          )
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Image.asset(
+          model.imagePath,
+          height: 300,
+          width: 300,
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
 }
